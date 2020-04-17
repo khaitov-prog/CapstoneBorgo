5899e567edcffb58d70270c162e935d98698d44a
khaitov-prog <60979562+khaitov-prog@users.noreply.github.com>
2020-04-17 14:46:50 -0400

    Add files via upload
    sprint 2

--- src/main/java/ca/sheridancollege/security/SecurityConfig.java
+++ src/main/java/ca/sheridancollege/security/SecurityConfig.java
@@ -1,25 +1,49 @@
 package ca.sheridancollege.security;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+
 	@Autowired
 	private LoginAccessDeniedHandler accessDeniedHandler;
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
+
+	}
+
+	@Autowired
+	UserDetailsServiceImp userDetailsService;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
 		http.authorizeRequests()
 		.antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/employee/**").hasAnyRole("ADMIN", "EMPLOYEE")
 		.antMatchers("/", "/images/**", "/css/**", "/js/**", "/**", "/users/**").permitAll()
+		.antMatchers("/h2-console/**").permitAll()
+		.antMatchers(HttpMethod.POST, "/admin/addUser")
+		.hasRole("ADMIN")
 		.anyRequest().authenticated()
 		.and().formLogin().loginPage("/login").permitAll()
 		.and().logout().invalidateHttpSession(true).clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
@@ -28,15 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		;
 	}
 	
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.
-		inMemoryAuthentication()
-		.passwordEncoder(NoOpPasswordEncoder.getInstance())
-		.withUser("Admin").password("password").roles("ADMIN")
-		.and()
-		.withUser("Employee").password("password").roles("EMPLOYEE"); 
-		}
+	
 	
 
 }
